248e1da1626b32c5fcfaa3f451b7f4e02d2190aa
Ashwin Dixit <ashwin@ownlifeful.com>
2016-12-17 12:15:51 -0500

    Initial Commit

---
+++ Quiz_6_exercise_6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Quiz_6_exercise_6
+//
+//  Created by Ashwin Dixit on 12/17/16.
+//  Copyright © 2016 Ashwin Dixit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
